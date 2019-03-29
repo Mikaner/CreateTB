e9d0b417d034e3e44ac96e84e682818eb4f30a06
Mikaner <c0118259dc@edu.teu.ac.jp>
2019-03-30 01:09:35 +0900

    update commands of play, remove, move and queue in MusicCog

--- music/MusicCog.py
+++ music/MusicCog.py
@@ -10,10 +10,11 @@ import urllib.parse as urlparse
 
 class MusicCog(commands.Cog):
     def __init__(self, bot):
-        self.Q = Queue()
         self.bot = bot
         self.voice_client = None
+        self.Q = Queue()
         self.setting = Settings()
+        self.download = Download()
 
     def is_url_valid(self, url):
         if url.startswith('https://www.youtube.com/watch?v='):
@@ -27,9 +28,15 @@ class MusicCog(commands.Cog):
     def status_queue(self, ctx):
         embed = discord.Embed(title='TB', description="The state of Queue:", color=0x00bfff)
         for index, job in enumerate(self.Q.get_queue()):
-            embed.add_field(name=index, value=job, inline=False)
+            embed.add_field(name=index+1, value=job, inline=False)
 
         return ctx.send(embed=embed)
+
+    def next(self):# need to adjust
+        if self.Q.get_queue() == []:
+            print("done")
+            return
+        #self.voice_client.play(self.Q.next_job(), after=self.next)
 
     @commands.command()
     async def join(self, ctx):
@@ -40,7 +47,7 @@ class MusicCog(commands.Cog):
         if ctx.author.voice is not None:
             voice_channel = ctx.author.voice.channel
         else:
-            await ctx.send('Join a voice channel before using $join')
+            await ctx.send('Please join a voice channel before using $join')
             return
 
         self.voice_client = await voice_channel.connect()
@@ -48,35 +55,36 @@ class MusicCog(commands.Cog):
     @commands.command()
     async def play(self, ctx, *args):
         if self.voice_client is None:
-            # if not joined
             if ctx.author.voice is not None:
                 voice_channel = ctx.author.voice.channel
             else:
-                await ctx.send('Join a voice channel before using $play')
+                await ctx.send('Please join a voice channel before using $play')
 
             self.voice_client = await voice_channel.connect()
 
         if len(args) == 0:
-            audio_source = discord.FFmpegPCMAudio('music/local_music_files/MikeTest.mp3')
+            self.Q.add_queue(discord.FFmpegPCMAudio('music/local_music_files/MikeTest.mp3'))
         elif len(args) == 1:
             # assert args is url
             is_valid, service = self.is_url_valid(args[0])
             if is_valid:
                 if service == 'youtube':
-                    download = Download()
-                    file_path = download.youtube_dl(args, self.setting.settings['download_file_ext'])
-                    audio_source = discord.FFmpegPCMAudio(file_path)
+                    file_path = self.download.youtube_dl(args, self.setting.settings['download_file_ext'])
+                    self.Q.add_queue(discord.FFmpegPCMAudio(file_path))
+
+
                 elif service == 'niconico':
-                    pass
+                    file_path = self.download.niconico_dl(args, self.setting.settings['download_file_ext'])
+                    self.Q.add_queue(discord.FFmpegPCMAudio(file_path))
             else:
                 # assert args is search words
                 pass
-            
         else:
             # assert args is search words
             pass
+
         if not self.voice_client.is_playing():
-            self.voice_client.play(audio_source, after=lambda e: print('done', e))
+            self.voice_client.play(self.Q.next_job(), after=self.next)
         
     @commands.command()
     async def stop(self, ctx):
@@ -102,11 +110,11 @@ class MusicCog(commands.Cog):
     async def remove(self, ctx, position):
         if self.voice_client is None:
             return
-        if position==0:
+        if int(position)==0:
             self.voice_client.stop()
         else:
             try:
-                self.Q.remove_queue(position)
+                self.Q.remove_queue(int(position))
             except IndexError:
                 await ctx.send("Out of queue.")
 
@@ -115,7 +123,12 @@ class MusicCog(commands.Cog):
 
     @commands.command()
     async def move(self,ctx,from_position,to_position):
-        self.Q.move_queue(from_position,to_position)
+        self.Q.move_queue(int(from_position),int(to_position))
+        await self.status_queue(ctx)
+
+    @commands.command()
+    async def queue(self,ctx):
+        await self.status_queue(ctx)
 
 
     @commands.command()
@@ -128,7 +141,7 @@ class MusicCog(commands.Cog):
     @commands.command()
     async def help(self, ctx):
 
-        embed = discord.Embed(title='TB', description="A music bot. List of commands are:", color=0xeee657)
+        embed = discord.Embed(title='TB', description="A music bot. List of commands are:", color=0x00bfff)
 
         for name, description in self.setting.settings['commands']:
             embed.add_field(name=name, value=description, inline=False)
