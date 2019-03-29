8d4cac3f4399afbbadb4ebf5eafa8b6e117d90de
Mikaner <c0118259dc@edu.teu.ac.jp>
2019-03-29 18:56:04 +0900

    bbb

--- MainTB.py
+++ MainTB.py
@@ -1,23 +1,19 @@
 import discord
 from discord.ext import commands
 
+from music.MusicCog import MusicCog
 import json
 import traceback
-
-INITIAL_COGS = [
-    'music.MusicCog'
-]
 
 
 class MainTB(commands.Bot):
     def __init__(self, command_prefix):
         super().__init__(command_prefix)
 
-        for cog in INITIAL_COGS:
-            try:
-                self.load_extension(cog)
-            except Exception:
-                traceback.print_exc()
+        try:
+            self.add_cog(MusicCog(self))
+        except Exception:
+            traceback.print_exc()
 
 
     async def on_ready(self):
