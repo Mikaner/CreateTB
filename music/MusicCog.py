45b05bc2abad56915d940d21b164aa00f51fb51e
hakomori64 <hakomori64@gmail.com>
2019-03-29 23:01:53 +0900

    play youtube music

--- music/MusicCog.py
+++ music/MusicCog.py
@@ -4,11 +4,14 @@ from music.Setting import Settings
 from music.Download import Download
 import json
 from discord.ext.commands import CommandNotFound
+import urllib.parse as urlparse
+
 
 class MusicCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.voice_client = None
+        self.setting = Settings()
 
     @commands.command()
     async def join(self, ctx):
@@ -16,25 +19,44 @@ class MusicCog(commands.Cog):
         if self.voice_client is not None and self.voice_client.is_connected():
             await self.voice_client.disconnect()
 
-        if ctx.author.voice.channel is not None:
+        if ctx.author.voice is not None:
             voice_channel = ctx.author.voice.channel
         else:
-            voice_channel = ctx.guild.voice_channels[0]
+            await ctx.send('Join a voice channel before using $join')
+            return
+
         self.voice_client = await voice_channel.connect()
 
     @commands.command()
     async def play(self, ctx, *args):
         if self.voice_client is None:
             # if not joined
-            if ctx.author.voice.channel is not None:
+            if ctx.author.voice is not None:
                 voice_channel = ctx.author.voice.channel
             else:
-                voice_channel = ctx.guild.voice_channels[0]
-                
+                await ctx.send('Join a voice channel before using $play')
+
             self.voice_client = await voice_channel.connect()
 
-        await ctx.send(args)
-        audio_source = discord.FFmpegPCMAudio('music/local_music_files/MikeTest.mp3')
+        if len(args) == 0:
+            audio_source = discord.FFmpegPCMAudio('music/local_music_files/MikeTest.mp3')
+        elif len(args) == 1:
+            # assert args is url
+            is_valid, service = self.is_url_valid(args[0])
+            if is_valid:
+                if service == 'youtube':
+                    download = Download()
+                    file_path = download.youtube_dl(args, self.setting.settings['download_file_ext'])
+                    audio_source = discord.FFmpegPCMAudio(file_path)
+                elif service == 'niconico':
+                    pass
+            else:
+                # assert args is search words
+                pass
+            
+        else:
+            # assert args is search words
+            pass
         if not self.voice_client.is_playing():
             self.voice_client.play(audio_source, after=lambda e: print('done', e))
         
@@ -73,15 +95,22 @@ class MusicCog(commands.Cog):
 
     @commands.command()
     async def help(self, ctx):
-        setting = Settings()
 
         embed = discord.Embed(title='TB', description="A music bot. List of commands are:", color=0xeee657)
 
-        for name, description in setting.settings['commands']:
+        for name, description in self.setting.settings['commands']:
             embed.add_field(name=name, value=description, inline=False)
 
         await ctx.send(embed=embed)
 
+    def is_url_valid(self, url):
+        if url.startswith('https://www.youtube.com/watch?v='):
+            return (True, 'youtube')
+        elif url.startswith('https://www.nicovideo.jp/watch/sm'):
+            return (True, 'niconico')
+        else:
+            return (False, '')
+        
 
 if __name__ == '__main__':
     prefix = "$"
