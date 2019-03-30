d79e20f52fa3df5048117abc8bbb406da402b8a5
hakomori64 <hakomori64@gmail.com>
2019-03-30 22:37:32 +0900

    reconnect url

--- music/MusicCog.py
+++ music/MusicCog.py
@@ -15,6 +15,7 @@ class MusicCog(commands.Cog):
         self.Q = Queue()
         self.setting = Settings()
         self.download = Download()
+        self.beforeArgs = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
 
     def is_url_valid(self, url):
         if url.startswith('https://www.youtube.com/watch?v='):
@@ -33,10 +34,14 @@ class MusicCog(commands.Cog):
         return ctx.send(embed=embed)
 
     def next(self):# need to adjust
+        print('load next audio')
         if self.Q.get_queue() == []:
             print("done")
             return
-        self.voice_client.play(self.Q.next_job(), after=lambda e: self.next())
+        try:
+            self.voice_client.play(self.Q.next_job(), after=lambda e: self.next())
+        except Exception:
+            print('error')
 
     @commands.command()
     async def join(self, ctx):
@@ -70,7 +75,7 @@ class MusicCog(commands.Cog):
             if is_valid:
                 if service == 'youtube':
                     file_path = self.download.youtube_stream(args, self.setting.settings['download_file_ext'])
-                    self.Q.add_queue(discord.FFmpegPCMAudio(file_path))
+                    self.Q.add_queue(discord.FFmpegPCMAudio(file_path, before_options=self.beforeArgs))
                 elif service == 'niconico':
                     file_path = self.download.niconico_dl(args, self.setting.settings['download_file_ext'])
                     self.Q.add_queue(discord.FFmpegPCMAudio(file_path))
@@ -103,7 +108,6 @@ class MusicCog(commands.Cog):
         if self.voice_client is None:
             return
         self.voice_client.stop()
-        self.next()
 
     @commands.command()
     async def remove(self, ctx, position):
