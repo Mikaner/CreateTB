9f9d8cfa52dfd1a06da128df2427af7228432beb
Glitch (mikaner-discord-testbot) <none>
2022-01-14 18:50:40 +0000

    👁️🌱 Updated with Glitch

--- services/music/README.md
+++ services/music/README.md
@@ -10,42 +10,49 @@ MikeTest.mp3 : 再生チェック用音声ファイル(assetsにも存在する)
   - dc
 - play (実装中)(使用可能)
   - p
-- playnow (実装中)(使用可能)
-- testplay (実装中)(使用可能)
+- playtop
+  - pt
+- testplay
+- search
 - skip
   - s
 - stop
 - start
-- np
-  - nowplaying
+- nowplaying
+  - np
 - list (実装中)(使用可能)
+  - ls
   - queue
   - q
-- loopqueue (実装中)(使用可能)
+- loopqueue
   -lq
-- clear (実装中)(使用不可)(バグ有)
-- remove (未実装)
+- clear
+- remove (実装中)(使用可能)
   - rm
+- move (実装中)(使用可能)
+  - mv
 
 ## タスク
 要件 実装 検証
-- Playの初期音量設定(20%) ☐ ☐
-- Join後のplayが動かないバグ除去 ☑ ☐
-- Skipのdestroy後にunplayed musicの呼び出し ☑ ☐
-- handleListの順番を固定化 ☐ ☐
-- Queueの割り込み ☐ ☐
-- Queueの個別削除 ☐ ☐
-- Queueの全削除 ☐ ☐
-- MikeTestのURLをplayNextが呼ぶと死ぬ
-- https://www.youtube.com/watch?v=gNp4VNr44hg ←これが再生されない ←410 Gone
+- Playの初期音量設定(20%) ☑ ☑
+- Join後のplayが動かないバグ除去 ☑ ☑
+- Skipのdestroy後にunplayed musicの呼び出し ☑ ☑
+- handleListの順番を固定化 ☑ ☑
+- rmの実装 ☑ ☑
+- Queueの全削除 ☑ ☐
+- Queueの個別削除 ☑ ☐
+- MikeTestのURLをplayNextが呼ぶと死ぬ ☑ ☐
 - handleListのindex情報の実装改善 ←処理速度の違いからずれてるっぽい ←awaitでできるかごにょってみたけど全然無理。どうすればええかわからん。
   - →forEachからforに直したらawaitが仕事し始めた ☑ ☐
-- handleListの表示改善(embedの利用) ☐ ☐
 - loopqueueの実装 ☑ ☐
-- 検索機能の実装 ☐ ☐
+- 検索機能の実装 ☑ ☐
+- loopqueueを設定したときのリアクション実装 ☑ ☐
+- moveの実装 ☑ ☐
+- handleListの表示改善(embedの利用) ☐ ☐
+- https://www.youtube.com/watch?v=gNp4VNr44hg ←これが再生されない ←410 Gone × □
 
 # 参考URL
 https://qiita.com/HungTran/items/98e2b07a0fdd9e9b37fd
 
-.pyの方<br>
-https://github.com/Mikaner/CreateTB
+ソースコードの方:<br>
+<a href="https://github.com/Mikaner/CreateTB">Github</a>
--- services/music/cog.js
+++ services/music/cog.js
@@ -1,6 +1,7 @@
 const {prefix} = require('../../config.json');
 const ytdl = require('ytdl-core');
-const {}
+const yts = require('yt-search');
+//const {handleTestSearch} = require('utils.js');
 
 
 module.exports = {
@@ -8,16 +9,16 @@ module.exports = {
   play: play,
   isValidCommand: isValidCommand,
   handlePlay: handlePlay,
+  handlePlayTop: handlePlayTop,
   handleSkip: handleSkip,
   handleStop: handleStop,
   handleStart: handleStart,
-  handlePlayNow: handlePlayNow,
+  handleRemove: handleRemove,
   handleDisconnect: handleDisconnect,
 }
 
-let servers = {};
-let connections = {};
-let is_pausing = false;
+const servers = {};
+const connections = {};
 
 function call(message){
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -27,9 +28,19 @@ function call(message){
     case 'testplay':
       handleTestPlay(message);
       break;
+    case 'join':
+      handleJoin(message);
+      break;
     case 'play':
     case 'p':
-      handlePlay(message,args[0]);
+      handlePlay(message,args);
+      break;
+    case 'playtop':
+    case 'pt':
+      handlePlayTop(message, args);
+      break;
+    case 'search':
+      handleSearch(message, args);
       break;
     case 'skip':
     case 's':
@@ -41,12 +52,6 @@ function call(message){
     case 'start':
       handleStart(message);
       break
-    case 'playnow':
-      handlePlayNow(message, args[0]);
-      break;
-    case 'join':
-      handleJoin(message);
-      break;
     case 'np':
     case 'nowplaying':
       handleNp(message);
@@ -56,12 +61,21 @@ function call(message){
       handleLoopQueue(message);
       break;
     case 'list':
+    case 'ls':
     case 'queue':
     case 'q':
       handleList(message);
       break;
     case 'clear':
       handleClear(message);
+      break;
+    case 'remove':
+    case 'rm':
+      handleRemove(message, args[0]);
+      break;
+    case 'move':
+    case 'mv':
+      handleMove(message, args[0], args[1]);
       break;
     case 'disconnect':
     case 'dc':
@@ -81,13 +95,21 @@ function isValidCommand(message, url){
     return false;
   }
   
+  return true;
+}
+
+function isYoutubeUrl(message, url){
   let reg = /^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/gm;
   if (!url.match(reg)) {
-    message.reply('Youtubeのリンクを判別できませんでした');
+    //message.reply('Youtubeのリンクを判別できませんでした');
     return false;
   }
   
   return true;
+}
+
+function makeServerStatus(){
+  return {queue: [], loopflag: false, is_pausing: false};
 }
 
 
@@ -109,6 +131,15 @@ function getFirstUnPlayedQueue(message){
   */
 }
 
+function getTitleByInfo(info, url){  // 削除候補
+  return `${info.player_response.videoDetails.title}`;//` \n Link: ${url}`;
+}
+
+async function getTitleByUrl(url){
+  const info = await ytdl.getBasicInfo(url);
+  return getTitleByInfo(info, url);
+}
+
 function popFirstUnPlayedQueue(message){
   return servers[message.guild.id].queue.shift();
 }
@@ -123,9 +154,10 @@ function playNext(server, message){
     play(connections[message.guild.id], message);
   } else {
     //handleDisconnect(message);
-    console.log(`The ${message.guild.id} was finished to play!`);
     console.log(servers[message.guild.id].dispatcher);
-    delete servers[message.guild.id];
+    console.log(`The ${message.guild.id} was finished to play!`);
+    servers[message.guild.id].dispatcher.destroy();
+    delete servers[message.guild.id].now_playing;
   }
 }
 
@@ -155,8 +187,7 @@ function handleJoin(message){
     message.member.voice.channel.join()
       .then(connection => {
         connections[message.guild.id] = connection;
-        if(!servers[message.guild.id]) { servers[message.guild.id] = {queue: []}; }
-        servers[message.guild.id].loopflag = false;
+        if(!servers[message.guild.id]) { servers[message.guild.id] = makeServerStatus(); }
       })
       .catch(console.log);
   }
@@ -167,6 +198,7 @@ function handleDisconnect(message){
   if(connections[message.guild.id]){
     connections[message.guild.id].disconnect();
     delete connections[message.guild.id];
+    delete servers[message.guild.id];
   }
   console.log(connections);
   console.log(servers[message.guild.id]);
@@ -174,16 +206,19 @@ function handleDisconnect(message){
 
 async function handleTestPlay(message){
   const url = 'https://cdn.glitch.me/0c4da0ec-8148-4d59-b605-4b229d189f4f/MikeTest.mp3?v=1640168216964';
+  const title = 'MikeTest.mp3';
   
-  if(!servers[message.guild.id]) { servers[message.guild.id] = {queue: []}; }
+  if(!servers[message.guild.id]) { servers[message.guild.id] = makeServerStatus(); }
   await servers[message.guild.id].queue.push({
     url: url,
     status: 0,
     local: true
   });
   
+  message.channel.send(`${title}を追加しました。`); 
+  
   console.log(message.guild.id);
-  console.log(servers[message.guild.id].queue);
+  console.log(servers[message.guild.id]);
   
   if (!connections[message.guild.id]) {
     message.member.voice.channel.join()
@@ -197,18 +232,34 @@ async function handleTestPlay(message){
   }
 }
 
-async function handlePlay(message, url){
+async function handlePlay(message, args){
+  let url = args[0];
+  let title;
   if(!isValidCommand(message, url)) { return; }
   
-  if(!servers[message.guild.id]) { servers[message.guild.id] = {queue: []}; }
+  if(!isYoutubeUrl(message, url)){
+    const r = await yts(args.join(' '));
+    const video = await r.videos.slice(0, 1)[0];
+    console.log('------- video:-----------');
+    console.log(video);
+    url = video.url;
+    title = video.title;
+  } else {
+    const info = await ytdl.getBasicInfo(url);
+    title = getTitleByInfo(info, url);// getInfoByUrlに変更候補
+  }
+  
+  if(!servers[message.guild.id]) { servers[message.guild.id] = makeServerStatus(); }
   await servers[message.guild.id].queue.push({
     url: url,
     status: 0,
     local: false
   });
   
+  message.channel.send(`${title}を追加しました。`); 
+  
   console.log(message.guild.id);
-  console.log(servers[message.guild.id].queue);
+  console.log(servers[message.guild.id]);
   
   if (!connections[message.guild.id]) {
     message.member.voice.channel.join()
@@ -222,6 +273,42 @@ async function handlePlay(message, url){
   }
 }
 
+async function handlePlayTop(message, args){
+  const server = servers[message.guild.id];
+  if(!server || !server.queue){
+    handlePlay(message, args);
+    return;
+  }
+  
+  let url = args[0];
+  
+  if(!isValidCommand(message, url)) { return; }
+  
+  let title;
+  
+  if(!isYoutubeUrl(message, url)){
+    const r = await yts(args.join(' '));
+    const video = await r.videos.slice(0, 1)[0];
+    console.log('------- video:-----------');
+    console.log(video);
+    url = video.url;
+    title = video.title;
+  } else {
+    const info = await ytdl.getBasicInfo(url);
+    title = getTitleByInfo(info, url);
+  }
+  
+  server.queue.splice(0, 0, {
+    url: url,
+    status: 0,
+    local: false
+  });
+  
+  message.channel.send(`${title}を次の曲に追加しました。`); // getInfoByUrlに変更候補
+  
+}
+
+
 function handleSkip(message){
   if (!servers[message.guild.id]) { return; }
   
@@ -234,33 +321,68 @@ function handleSkip(message){
 
 function handleStop(message){
   if (connections[message.guild.id]) {
-    if(!is_pausing){
-      is_pausing = true;
-      servers[message.guild.id].dispatcher.pause(true);
+    const server = servers[message.guild.id];
+    if(!server.is_pausing){
+      server.is_pausing = true;
+      server.dispatcher.pause(true);
     }
   }
 }
 
 function handleStart(message){
   if (connections[message.guild.id]) {
-    if(is_pausing){
-      is_pausing = false;
-      servers[message.guild.id].dispatcher.resume();
+    const server = servers[message.guild.id];
+    if(server.is_pausing){
+      server.is_pausing = false;
+      server.dispatcher.resume();
     }
+  }
+}
+
+function handleRemove(message, index_num){
+  let index = Number(index_num);
+  if(index_num.toLowerCase()==="next"){index=0}
+  if(index===NaN){message.reply("数値を入力してください。");return;}
+  const server = servers[message.guild.id];
+  if (server && server.queue.length){
+    message.reply(`${server.queue.splice(index%server.queue.length, 1)[0].url} was removed !`);
+  }
+}
+
+function handleMove(message, from_index, to_index){
+  let f_i = Number(from_index);
+  let t_i = Number(to_index);
+  if(from_index.toLowerCase()==="next"){f_i=0}
+  if(to_index.toLowerCase()==="next"){t_i=0}
+  if(f_i===NaN || t_i===NaN){message.reply("数値を入力してください。");return;}
+  
+  const server = servers[message.guild.id];
+  
+  if(server && server.queue.length){
+    f_i = f_i%server.queue.length;
+    t_i = t_i%server.queue.length;
+  
+    const target = server.queue.splice(f_i, 1)[0];
+    console.log(`f_i is ${f_i} \nt_i is ${t_i}\ntarget is ${target}`);
+    server.queue.splice(t_i, 0, target);
+    message.reply(`${target.url} was moved from ${f_i} to ${t_i}`);
   }
 }
 
 function handleClear(message){
   if (servers[message.guild.id]){
-    delete servers[message.guild.id].queue
+    delete servers[message.guild.id].queue;
     servers[message.guild.id].queue = [];
   }
 }
 
 function handleLoopQueue(message){
-  servers[message.guild.id].loopflag = !servers[message.guild.id].loopflag;
+  const loopflag = !servers[message.guild.id].loopflag;
+  servers[message.guild.id].loopflag = loopflag;
+  message.channel.send(`Turned ${loopflag?"on":"off"} the loop.`);
 }
 
+/*
 function handlePlayNow(message, url){
   let server = servers[message.guild.id];
   if(!server){
@@ -274,18 +396,23 @@ function handlePlayNow(message, url){
   });
   server.dispatcher.destroy();
 }
+*/
 
 async function handleNp(message){
   let server = servers[message.guild.id];
-  if(!server || !server.queue){
+  if(!server || !server.now_playing){
     message.reply("何も再生していません");
     return;
   }
   let current_queue = getCurrentQueue(message);//server.queue);
   if(!server) {return;}
-  ytdl.getBasicInfo(current_queue.url).then(info => {
-    message.channel.send(getInfoMsg(info, current_queue.url));
-  });
+  if(current_queue.local){
+    message.channel.send(" Title: MikeTest.mp3");
+  } else {
+    ytdl.getBasicInfo(current_queue.url).then(info => {
+      message.channel.send(" Title: "+getTitleByInfo(info, current_queue.url));
+    });
+  }
 }
 
 async function handleList(message){
@@ -299,7 +426,7 @@ async function handleList(message){
     console.log(queue);
     if(!queue.local){
       const info = await ytdl.getBasicInfo(queue.url);
-      msg += `${index===0?"Next":index}`+getInfoMsg(info, queue.url) + '\n -------------------------\n';
+      msg += `${index===0?"Next":index} Title: `+getTitleByInfo(info, queue.url) + '\n -------------------------\n';  // getTitleByUrlに変更候補
       console.log(msg);
     }else{
       msg += `${index===0?"Next":index} Title: MikeTest.mp3\n -------------------------\n`;
@@ -308,7 +435,6 @@ async function handleList(message){
     console.log("test");
   };
   
-  //await delay();
   
   if(msg===""){
     message.reply("何もキューに入ってないです");
@@ -317,10 +443,20 @@ async function handleList(message){
   }
 }
 
-function getInfoMsg(info, url){
-  return ` Title: ${info.player_response.videoDetails.title}`;//` \n Link: ${url}`;
-}
-
-function delay(){
-  return new Promise(resolve => setTimeout(resolve, 1000));
+async function handleSearch(message, args){
+  console.log(args);
+  
+  const video_max_length = 3;
+  
+  const r = await yts(args.join(' '));
+  const videos = r.videos.slice(0, video_max_length);
+  
+  let msg = "検索結果:\n========== \n";
+  videos.forEach( (v) => {
+    const views = String(v.views).padStart(10, ' ');
+    msg += (`${ v.title } (${v.timestamp}) | ${v.author.name}\n`);
+    msg += (`url: ${v.url}\n---------- \n`);
+  });
+  
+  message.channel.send(msg);
 }
